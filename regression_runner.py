52c2ca9088b505ba57dd56ff1473c7ff709c0dfc
kikibox <ksb93@cornell.edu>
2017-08-22 15:25:39 -0400

    regression changes

--- regression_runner.py
+++ regression_runner.py
@@ -8,6 +8,8 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
+import combination
+
 
 def read_in_table(configs):
 	file = 'MTA-Bus-Time_.'+configs['network_date']+'.txt'
@@ -22,25 +24,38 @@ def main():
 
 	print(configs)
 
-	with open('networks_by_date/network_'+configs['training_date']+'_with_duration'+'.pickle', 'rb') as f:
-		network_training = pickle.load(f)
+	with open('networks_by_date/network_'+configs['training_date_1']+'_with_duration'+'.pickle', 'rb') as f:
+		network_training_1 = pickle.load(f)
 
-	with open('networks_by_date/network_'+configs['testing_date']+'_with_duration'+'.pickle', 'rb') as f:
+	with open('networks_by_date/network_'+configs['training_date_2']+'.pickle', 'rb') as f:
+		network_training_2 = pickle.load(f)
+
+	with open('networks_by_date/network_'+configs['training_date_3']+'_with_duration'+'.pickle', 'rb') as f:
+		network_training_2 = pickle.load(f)
+
+	network_training = combination.combine_two_networks(network_training_1, network_training_2)
+
+	print(network_training)
+
+
+	with open('networks_by_date/network_'+configs['testing_date']+'.pickle', 'rb') as f:
 		network_testing = pickle.load(f)
 		
 
 	#regression_utils.iterate_stops(network_training, network_testing)
-	color = 'bgrmck'
+	color = 'bgrmckg'
 	for idx, previous_stop in enumerate(configs['previous_stop_list']):
 		stop = configs['stop_list'][idx]
 		info =  regression_utils.run_configs_stops(network_training, network_testing, stop, previous_stop)
 		points_point_x = list(info.keys())
 		points_point_y = []
-		print(info)
+		#print(info)
 		for p in points_point_x:
 			points_point_y.append(info[p]['error']/ info[p]['mean'])
+		print(stop)
 		print(points_point_x)
 		print(points_point_y)
+		print(info)
 		plt.plot(points_point_x, points_point_y, color[idx]+'-')
 
 	plt.savefig('saved_lakshmi_2.png')
